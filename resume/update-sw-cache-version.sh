57744676820e1f15c72946c4427a478f593fb531
Sid Vishnoi <sidvishnoi8@gmail.com>
2019-07-31 00:53:09 +0530

    move resume to sidvishnoi/resume repo

--- resume/update-sw-cache-version.sh
+++
@@ -1,6 +0,0 @@
-hash=$(
-  cat resume/index.html resume/resume.css |
-  git hash-object --stdin |
-  xargs -i git rev-parse --short {}
-)
-sed -i "s/const __version.*/const __version = \"${hash}\";/" resume/resume-service-worker.js
