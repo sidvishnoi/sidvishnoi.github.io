fa566bd9da14d57a583cb725391a65301e02d67f
Sid Vishnoi <sidvishnoi8@gmail.com>
2019-12-29 17:05:04 +0530

    Redesign (#1)

--- script.js
+++ script.js
@@ -1,88 +1,29 @@
-class DarkModeSwitcher extends HTMLElement {
-  constructor() {
-    super();
-    const shadowRoot = this.attachShadow({ mode: "open" });
-    shadowRoot.innerHTML = `
-      <style>
-        .theme-switch-wrapper {
-          display: flex;
-          align-items: center;
-        }
-        .theme-switch {
-          display: inline-block;
-          height: 1.4rem;
-          position: relative;
-          width: 2.5rem;
-        }
+const $id = id => document.getElementById(id);
 
-        .theme-switch input {
-          display: none;
-        }
+const preferences = document.forms.namedItem('preferences');
 
-        .slider {
-          background-color: #fff;
-          bottom: 0;
-          cursor: pointer;
-          left: 0;
-          position: absolute;
-          right: 0;
-          top: 0;
-          transition: 0.4s;
-        }
+const setTheme = theme => {
+  localStorage.setItem('theme', theme);
+  document.documentElement.dataset.theme = theme;
+};
 
-        .slider:before {
-          background-color: #000;
-          bottom: 0.2rem;
-          content: "";
-          height: 1rem;
-          left: 0.2rem;
-          position: absolute;
-          transition: 0.4s;
-          width: 1rem;
-        }
+const currentTheme =
+  localStorage.getItem('theme') ||
+  (matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light');
+preferences.theme.value = currentTheme;
+setTheme(currentTheme);
 
-        input:checked + .slider {
-          background-color: yellow;
-        }
-
-        input:checked + .slider:before {
-          transform: translateX(100%);
-        }
-      </style>
-      <div class="theme-switch-wrapper" aria-hidden="true">
-        <label class="theme-switch" for="checkbox" title="Toggle dark mode">
-          <input type="checkbox" id="checkbox" />
-          <div class="slider"></div>
-        </label>
-      </div>
-    `;
-
-    const toggler = shadowRoot.querySelector('input[type="checkbox"]');
-    const current = localStorage.getItem("theme") || null;
-
-    if (
-      matchMedia("prefers-color-scheme: dark").matches ||
-      current === "dark"
-    ) {
-      toggler.checked = true;
-      this.setDarkTheme();
-    }
-    toggler.addEventListener("change", this.switchTheme.bind(this), false);
+preferences.addEventListener('change', event => {
+  if (event.target.name === 'theme') {
+    setTheme(preferences.theme.value);
   }
+});
 
-  switchTheme(e) {
-    e.target.checked ? this.setDarkTheme() : this.setLightTheme();
+$id('preferences-menu-toggle').addEventListener('click', () => {
+  const isVisible = $id('preferences').hidden;
+  $id('preferences').hidden = !isVisible;
+  if (isVisible) {
+    preferences.tabIndex = 0;
+    preferences.focus();
   }
-
-  setDarkTheme() {
-    document.documentElement.setAttribute("data-theme", "dark");
-    localStorage.setItem("theme", "dark");
-  }
-
-  setLightTheme() {
-    document.documentElement.setAttribute("data-theme", "light");
-    localStorage.setItem("theme", "light");
-  }
-}
-
-customElements.define("dark-mode-switcher", DarkModeSwitcher);
+});
